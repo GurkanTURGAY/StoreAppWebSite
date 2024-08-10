98a546fb07b4e2a5b740b3090763c649c5f44dba
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-10 16:12:07 +0300

    StoreAppWebSite

---
+++ Migrations/20240809161754_Productone.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Productone : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Price", "ProductName" },
+                values: new object[] { 6, 40000m, "HeadPhone" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 6);
+        }
+    }
+}
---
+++ Models/RepositoryContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApp.Models
+{
+    public class RepositoryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public RepositoryContext(DbContextOptions<RepositoryContext> options)
+        : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .HasData(
+                new Product() { ProductId = 1, ProductName = "Computer", Price = 25000 },
+                new Product() { ProductId = 2, ProductName = "Keyboard", Price = 5000 },
+                new Product() { ProductId = 3, ProductName = "Mouse", Price = 10000 },
+                new Product() { ProductId = 4, ProductName = "Monitor", Price = 15000 },
+                new Product() { ProductId = 5, ProductName = "Deck", Price = 30000 },
+                new Product() { ProductId = 6, ProductName = "HeadPhone", Price = 40000 }
+            );
+        }
+    }
+}
