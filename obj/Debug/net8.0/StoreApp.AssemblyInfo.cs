ce71f35a1aad583d966c5cc2f93b46c7a2365cd9
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-11 19:19:04 +0300

    interfaces

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ba626a094e1797bc4d514c5e34ba4802d27b7e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b64dc5f63cd2ecfb7c1e0f5d7fdd43fd0496647")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
