1a57412206e4ee895d767b62fdde69f5fb097cce
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-11 15:09:49 +0300

    bootstrap grid

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96bd1031641e64b89b0de98cd99f25d46d0818a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ac32987acbc35e2fd437e745f02f5b998250ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
