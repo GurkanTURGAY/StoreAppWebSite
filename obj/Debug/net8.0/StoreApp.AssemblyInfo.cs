e574d121fc3d95a3fef4faefdbea34f3799cbeac
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-12 18:24:24 +0300

    Admin Layout

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55233c7b3b6fe88763a64b142e694cd31f297eb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2dce0c32085115527f01d80658d08843949cb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
