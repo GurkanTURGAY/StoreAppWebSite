8def1dbd0ca0b7d46f7b63947d1a76b2da3e5735
Gurkan TURGAY <gurkan.turgay29@gmail.com>
2024-08-12 18:46:43 +0300

    Admin Navbar

--- obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StoreApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2dce0c32085115527f01d80658d08843949cb6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e574d121fc3d95a3fef4faefdbea34f3799cbeac")]
 [assembly: System.Reflection.AssemblyProductAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
